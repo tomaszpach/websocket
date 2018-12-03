4a221f3080e2d8d59738715967615db31358eb32
tomaszpach <tomaszpach@icloud.com>
2018-12-03 19:56:19 +0100

    Updates

---
+++ precache-manifest.e508de565d559097615749dd05b705da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77d4f6e267576ef971cd",
+    "url": "/crypto/static/css/main.a237bc37.chunk.css"
+  },
+  {
+    "revision": "77d4f6e267576ef971cd",
+    "url": "/crypto/static/js/main.77d4f6e2.chunk.js"
+  },
+  {
+    "revision": "74f6db466cb4b0dc6e96",
+    "url": "/crypto/static/js/1.74f6db46.chunk.js"
+  },
+  {
+    "revision": "074583b5304c3138ac11",
+    "url": "/crypto/static/js/runtime~main.074583b5.js"
+  },
+  {
+    "revision": "2179e751a01ef19b3f15b39b7304ba73",
+    "url": "/crypto/index.html"
+  }
+];
