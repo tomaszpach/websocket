bdf1638a2462ada3bd36faa149ea1defd814f306
tomaszpach <tomaszpach@icloud.com>
2018-12-05 00:30:32 +0100

    usuniecie zbednych styli

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import {Provider} from 'react-redux';
 
 import rootReducer from './reducers/rootReducer';
 
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
