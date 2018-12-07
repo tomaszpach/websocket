446b6b9a1759834affa4e7065c2ef2414cddc283
Tomasz Pach <tomaszpach@icloud.com>
2018-12-07 14:40:55 +0100

    header 'logo' animation in

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            <h2 className="site-title">BootyBay</h2>
+            <h2 className="site-title">
+                <span className="text1">Booty</span>
+                <span className="text2">Bay</span>
+            </h2>
         </header>
     );
 };
