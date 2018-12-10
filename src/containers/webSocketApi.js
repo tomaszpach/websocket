1245315c8c5b208cf523e7cc3954ba80ef5936ce
Tomasz Pach <tomaszpach@icloud.com>
2018-12-10 15:22:13 +0100

    przygotowanie aktualizacji orderbooka

--- src/containers/webSocketApi.js
+++ src/containers/webSocketApi.js
@@ -3,6 +3,9 @@ import {connect} from 'react-redux';
 import axios from "axios";
 
 class WebSocketApi extends Component {
+    state = {
+        array: []
+    };
 
     fetchApi() {
         this.props.toggleLoader(true);
@@ -23,24 +26,69 @@ class WebSocketApi extends Component {
     websocket() {
         const webSocket = new WebSocket('wss://api2.bitbay.net/websocket/');
         webSocket.onopen = () => {
-            webSocket.send(`{"action": "subscribe-public", "module": "trading", "path": "ticker"}`)
+            webSocket.send(JSON.stringify({
+                action: "subscribe-public",
+                module: "trading",
+                path: "ticker"
+            }));
+            webSocket.send(JSON.stringify({
+                action: "subscribe-public",
+                module: "trading",
+                path: "orderbook/BTC-PLN"
+            }));
+
+            webSocket.send(`{"action": "subscribe-public", "module": "trading", "path": "ticker"}`);
         };
 
         webSocket.onmessage = e => {
-            const data = e.data,
-                parsedData = JSON.parse(data);
+            const data = JSON.parse(e.data);
 
-            if (parsedData.hasOwnProperty('message')) {
-                if (parsedData.message.market.code === this.props.state.currency) {
-                    this.props.updateWebsocket(parsedData.message);
+            // console.log(data.topic, data);
+
+            // Aktualizacja dla tickera (tego na samej gorze)
+            if (data.topic === 'trading/ticker') {
+                if (data.hasOwnProperty('message')) {
+                    if (data.message.market.code === this.props.state.currency) {
+                        this.props.updateWebsocket(data.message);
+                    }
                 }
             }
+
+            // if (data.topic === 'trading/orderbook/btc-pln') {
+            //     if (data.hasOwnProperty('message')) {
+            //         if (data.message.changes.length > 0) {
+            //             for (let i=0; i < data.message.changes.length; i++) {
+            //                 if (data.message.changes[i].action === 'remove' && data.message.changes[i].entryType === 'Buy') {
+            //                     // console.log('data.message.changes[i]', data.message.changes[i]);
+            //                     this.doSomethingOnChange(`remove ${i}`, data.message.changes[i]);
+            //                 }
+            //             }
+            //
+            //         }
+            //         // if (data.message.market.code === this.props.state.currency) {
+            //         //     this.props.updateWebsocket(data.message);
+            //         // }
+            //     }
+            // }
         };
+    }
+
+    doSomethingOnChange(type, data) {
+        let array = this.state.array;
+        array.push(data);
+        // console.log(type, data);
+
+        this.setState({array});
+        if (this.state.array.length === 10) {
+            console.log('this.state.array', this.state.array);
+        }
+
     }
 
     componentDidMount() {
         this.fetchApi();
         this.websocket();
+        // this.websocketOrderbook();
     }
 
     componentDidUpdate(prevProps) {
